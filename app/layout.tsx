c5087fe57098ae4302bc227ea79c26a7804cdf3c
sai vishruth <saivishruth183@gmail.com>
2025-10-23 10:02:06 +0530

    Updated project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="public\favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body className={inter.className}>
         <Header />
